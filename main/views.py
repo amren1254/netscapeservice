356abd79751e87bc18f6e45005478940dcc0c0fc
anuj <anuj85500@gmail.com>
2024-02-18 22:00:13 +0530

    added more html contents

--- main/views.py
+++ main/views.py
@@ -9,10 +9,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def homepage(request):
-    return render(request=request,
-                  template_name='main/home.html',
-                  context={"tutorials": Tutorial.objects.all})
+# def homepage(request):
+#     return render(request=request,
+#                   template_name='main/home.html',
+#                   context={"tutorials": Tutorial.objects.all})
 
 
 def homepage(request):
