1c94bf6bb16fcd205a68045ecb69ac525761fbe7
Anuj Thakur <40914997+anujkumarthakur@users.noreply.github.com>
2024-03-07 20:18:07 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from tinymce.models import HTMLField
 
 
 class TutorialCategory(models.Model):
@@ -35,9 +36,8 @@ class TutorialSeries(models.Model):
 
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
-    tutorial_content = models.TextField()
+    tutorial_content = HTMLField()
     tutorial_published = models.DateTimeField('date published')
-    # https://docs.djangoproject.com/en/2.1/ref/models/fields/#django.db.models.ForeignKey.on_delete
     tutorial_series = models.ForeignKey(
         TutorialSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
     tutorial_slug = models.CharField(max_length=200, default=1)
